e56c7c307b8ae677fb460117a89131a93e17c065
Younes-sakhnou <younes.sh1234@gmail.com>
2023-09-01 01:30:41 +0000

    Fixing betty style.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,21 +8,21 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        int left = 1, right = 1;
+	if (tree)
+	{
+		int left = 1, right = 1;
 
-        if (tree->right)
-            right += binary_tree_height(tree->right);
-        if (tree->left)
-            left += binary_tree_height(tree->left);
-        if (left > right)
-            return (left);
-        else
-            return (right);
-    }
-    else
-        return (0);
+		if (tree->right)
+			right += binary_tree_height(tree->right);
+		if (tree->left)
+			left += binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
+		return (0);
 }
 
 /**
@@ -33,8 +33,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (binary_tree_height(tree->left)
-            - binary_tree_height(tree->right));
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_height(tree->left)
+			- binary_tree_height(tree->right));
 }
